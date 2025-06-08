a9177a93a81a1eac3a0b25c08de688355d27d4d5
David Bernard <david.bernard.31@gmail.com>
2025-06-08 19:30:14 +0200

    feat(site): enable comment on documentation via Giscus
    [How to Integrate Giscus Comments into VitePress | aiktb](https://aiktb.dev/blog/giscus-with-vitepress)

--- cdviz-site/.vitepress/theme/index.ts
+++ cdviz-site/.vitepress/theme/index.ts
@@ -1,8 +1,11 @@
 // https://vitepress.dev/guide/custom-theme
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import Layout from './Layout.vue';
 import "./style.css";
 
 export default {
   extends: DefaultTheme,
+  // https://vitepress.dev/guide/extending-default-theme#layout-slots
+  Layout,
 } satisfies Theme;
